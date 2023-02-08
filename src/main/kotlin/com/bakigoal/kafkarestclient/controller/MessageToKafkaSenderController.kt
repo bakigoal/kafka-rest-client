fdf423d432993c094baedefdd176670a17a050f2
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-02-08 17:50:34 +0300

    send bulk messages

--- src/main/kotlin/com/bakigoal/kafkarestclient/controller/MessageToKafkaSenderController.kt
+++ src/main/kotlin/com/bakigoal/kafkarestclient/controller/MessageToKafkaSenderController.kt
@@ -1,17 +1,29 @@
 package com.bakigoal.kafkarestclient.controller
 
 import com.bakigoal.kafkarestclient.kafka.producer.MessageSender
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/kafka")
+@RequestMapping("/kafka/topic")
 class MessageToKafkaSenderController(
     private val messageSender: MessageSender
 ) {
 
-    @PostMapping("/{topic}/send")
-    fun send(@PathVariable topic: String, @RequestBody message: String): String {
-        messageSender.send(topic, message)
-        return "Message to topic $topic is sent: \n$message"
+    val log: Logger = LoggerFactory.getLogger(MessageToKafkaSenderController::class.java)
+
+    @PostMapping("/{topic}")
+    fun send(
+        @PathVariable topic: String,
+        @RequestBody message: String,
+        @RequestHeader(required = false, name = "count") count: Int?,
+    ): String {
+        log.info("Count: $count")
+
+        repeat(count?:1) {
+            messageSender.send(topic, message)
+        }
+        return "Messages($count) to topic $topic is sent: \n$message"
     }
 }
